ab0df3ea02db9a7d1473be43e512721e90ade022
mishatron <mish_k98@mail.ru>
2019-07-08 21:34:23 +0300

    some constants

--- lib/fakedart.dart
+++ lib/fakedart.dart
@@ -1,8 +1,43 @@
 library fakedart;
 
-/// A Calculator.
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:yaml/yaml.dart';
+
+/// A Fakedart class.
 class Fakedart {
-  static bool getUniqueValue(){
+  final Locale locale;
+
+  final numeralAndBracesRegEx = "#\\{(.*?)\\}";
+  final numeralRegEx = ".*#(\\{[^a-zA-z]|[^{])+";
+  final numeralOnlyRegEx = "#";
+  final defaultLanguage = "en";
+
+//  val yaml = Yaml()
+  LinkedHashMap<String, LinkedHashMap<String, String>> fakeValues;
+  LinkedHashMap<String, LinkedHashMap<String, String>> fakeValuesDefaults;
+
+  bool uniqueValueActive = false;
+
+  Fakedart(this.locale){
+    var stringLocale = locale.languageCode;
+
+    if (locale.countryCode.isNotEmpty) {
+      stringLocale = stringLocale+=(locale.countryCode.toLowerCase());
+    }
+
+    //TODO
+//    this.fakeValues = getValues(stringLocale);
+    if (locale.languageCode != defaultLanguage) {
+//      this.fakeValuesDefaults = getValues(defaultLanguage);
+    } else {
+      this.fakeValuesDefaults = LinkedHashMap();
+    }
+  }
+
+
+  static bool getUniqueValue() {
     return true;
   }
 }
