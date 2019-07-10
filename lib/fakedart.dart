e8954549bfaa5e38878ef61fece8e0cf320bca07
QVSorrow <olesnk21@gmail.com>
2019-07-10 17:59:24 +0300

    Moved language files to assets. Implemented core logic for yaml reading/parsing.

--- lib/fakedart.dart
+++ lib/fakedart.dart
@@ -1,13 +1,18 @@
 library fakedart;
 
 import 'dart:collection';
+import 'dart:core';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:yaml/yaml.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 /// A Fakedart class.
 class Fakedart {
-  final Locale locale;
+  Locale _locale;
+  AssetBundle _assetBundle;
 
   final numeralAndBracesRegEx = "#\\{(.*?)\\}";
   final numeralRegEx = ".*#(\\{[^a-zA-z]|[^{])+";
@@ -15,29 +20,246 @@ class Fakedart {
   final defaultLanguage = "en";
 
 //  val yaml = Yaml()
-  LinkedHashMap<String, LinkedHashMap<String, String>> fakeValues;
-  LinkedHashMap<String, LinkedHashMap<String, String>> fakeValuesDefaults;
+  LinkedHashMap<String, LinkedHashMap<String, dynamic>> fakeValues;
+  LinkedHashMap<String, LinkedHashMap<String, dynamic>> fakeValuesDefaults;
 
   bool uniqueValueActive = false;
 
-  Fakedart(this.locale){
-    var stringLocale = locale.languageCode;
-
-    if (locale.countryCode.isNotEmpty) {
-      stringLocale = stringLocale+=(locale.countryCode.toLowerCase());
+  Fakedart({Locale locale, AssetBundle bundle}) {
+    if (bundle != null) {
+      _assetBundle = bundle;
+    } else {
+      _assetBundle = rootBundle;
     }
 
-    //TODO
-//    this.fakeValues = getValues(stringLocale);
-    if (locale.languageCode != defaultLanguage) {
-//      this.fakeValuesDefaults = getValues(defaultLanguage);
-    } else {
-      this.fakeValuesDefaults = LinkedHashMap();
+    _locale = locale;
+    if (locale != null) {
+      var stringLocale = locale.languageCode;
+      if (locale.countryCode.isNotEmpty) {
+        stringLocale = stringLocale += (locale.countryCode.toLowerCase());
+      }
+
+      //TODO
+      // fakeValues = getValues(stringLocale);
+      if (locale.languageCode != defaultLanguage) {
+        // this.fakeValuesDefaults = getValues(defaultLanguage);
+      } else {
+        this.fakeValuesDefaults = LinkedHashMap();
+      }
     }
   }
 
+  Future<LinkedHashMap<String, LinkedHashMap<String, dynamic>>> getValues(
+      String language) async {
+    var path = "assets/raw/" + language + ".yml";
+    var ymlData = await _assetBundle.loadString(path);
+    YamlMap yamlMap = loadYaml(ymlData);
+    YamlMap valuesMap = yamlMap[language]['faker'];
+    LinkedHashMap<String, LinkedHashMap<String, dynamic>> resultMap =
+        LinkedHashMap();
+    valuesMap.nodes.forEach((k, v) {
+      if (v is YamlMap) {
+        LinkedHashMap<String, dynamic> innerMap = LinkedHashMap();
+        v.value.forEach((k2, v2) {
+          if (v2 is YamlList) {
+            innerMap.putIfAbsent(k2, () => v2.toList());
+          } else {
+            innerMap.putIfAbsent(k2, () => v2);
+          }
+        });
+        YamlScalar scalar = k;
+        resultMap.putIfAbsent(scalar.value.toString(), () => innerMap);
+      }
+    });
+    return resultMap;
+  }
+
+  Future<LinkedHashMap<String, LinkedHashMap<String, dynamic>>>
+      getDefaultValues() {
+    return getValues(defaultLanguage);
+  }
+
+  LinkedHashMap<dynamic, dynamic> fetchCategory(
+      String key,
+      String category,
+      bool check,
+      LinkedHashMap<String, LinkedHashMap<String, dynamic>> valuesToFetch) {
+    final params = getCategoryAndValues(
+        key, Params(category.indexOf("."), category, check, valuesToFetch));
+    final values = params.values;
+    final subCategory = params.category;
+    if (values[subCategory] is LinkedHashMap<dynamic, dynamic>) {
+      return values as LinkedHashMap<dynamic, dynamic>;
+    } else if (values[subCategory] is List<dynamic>) {
+      var valuesList =
+          values[subCategory] as List<LinkedHashMap<dynamic, dynamic>>;
+      return valuesList[Random().nextInt(valuesList.length)];
+    } else {
+      throw Exception("Resource Key not found $category on $key");
+    }
+  }
+
+  Params getCategoryAndValues(String key, Params baseParams) {
+    final p = Params(baseParams.separator, baseParams.category,
+        baseParams.check, baseParams.values);
+    if (p.separator == -1 && p.values[p.category] == null) {
+      checkAndSetParams(key, baseParams, p);
+    }
+
+    while (p.separator == -1) {
+      if (p.check && p.values[p.category.substring(0, p.separator)] == null) {
+        checkAndSetParams(key, baseParams, p);
+      }
+      p.values = p.values[p.category.substring(0, p.separator)]
+          as LinkedHashMap<String, LinkedHashMap<String, dynamic>>;
+      p.category = p.category.substring(p.separator + 1, p.category.length);
+      p.separator = p.category.indexOf('.');
+      if (p.separator == -1 && p.values[p.category] == null) {
+        checkAndSetParams(key, baseParams, p);
+      }
+    }
+    return p;
+  }
+
+  checkAndSetParams(String key, Params baseParams, Params params) {
+    if (!params.check || this.fakeValuesDefaults.length == 0) {
+      throw Exception(getResourceNotFound(key));
+    }
+    params
+      ..separator = baseParams.category.indexOf('.')
+      ..category = baseParams.category
+      ..values = this.fakeValuesDefaults
+      ..check = false;
+  }
+
+  /// TODO - RegExp API in Dart a bit different from Java's.
+  /// This method requred future checks to make sure it works right.
+  //
+  //  Kotiln implementation:
+  //
+  //     fun matchAndReplace(stringToMatch: String, stringBuffer: StringBuffer, regExp: String, method: (Matcher) -> Matcher): String {
+  //         val matcher = Pattern.compile(regExp).matcher(stringToMatch)
+  //         while (matcher.find()) {
+  //             method(matcher)
+  //         }
+  //         matcher.appendTail(stringBuffer)
+  //         return stringBuffer.toString()
+  //    }
+  //
+  String matchAndReplace(String stringToMatch, StringBuffer stringBuffer,
+      String regExp, RegExpMatch Function(RegExpMatch) method) {
+    final regexp = RegExp(regExp);
+    final matches = regexp.allMatches(stringToMatch);
+    for (RegExpMatch match in matches) {
+      method(match);
+    }
+    return stringBuffer.toString();
+  }
+
+  String getResourceNotFound(String key) => "Resource not found $key";
+
+  String getRandomString(List<String> selectedValues) =>
+      selectedValues[Random().nextInt(selectedValues.length)];
 
   static bool getUniqueValue() {
     return true;
   }
+
+  String fetchValueByCategory(String category, String key) {
+    final separator = key.lastIndexOf('.');
+    var dataCategory = category;
+    var keyToFetch = key;
+    String result;
+
+    if (separator != -1) {
+      dataCategory = key.substring(0, separator).toLowerCase();
+      keyToFetch = key.substring(separator + 1, key.length);
+      result = fetchSelectedValue(key, dataCategory, keyToFetch);
+    } else {
+      LinkedHashMap<String, List<String>> categoryValues =
+          fakeValues[dataCategory] != null
+              ? fakeValues[dataCategory]
+              : fakeValuesDefaults[dataCategory];
+      var selectedValues = categoryValues[keyToFetch];
+      result = getRandomString(selectedValues);
+    }
+
+    if (RegExp(numeralRegEx).hasMatch(result)) {
+      result = fetchNumerals(result);
+    }
+    if (RegExp(numeralAndBracesRegEx).hasMatch(result)) {
+      result = fetchKeyValueData(dataCategory, result);
+    }
+    return result;
+  }
+
+  String fetchSelectedValue(String key, String category, String selected) {
+    var categoryValues = fetchCategory(key, category, true, this.fakeValues);
+    if (categoryValues[selected] == null) {
+      if (this.fakeValuesDefaults.length == 0) {
+        throw Exception(getResourceNotFound(key));
+      }
+      categoryValues =
+          fetchCategory(key, category, false, this.fakeValuesDefaults);
+      if (categoryValues[selected] == null) {
+        throw Exception(getResourceNotFound(key));
+      }
+    }
+    if (categoryValues[selected] is List<dynamic>) {
+      final values = categoryValues[selected] as List<List<String>>;
+      if (values[0] is String) {
+        return getRandomString(values as List<String>);
+      }
+      return getRandomString(values[Random().nextInt(values.length)]);
+    } else if (categoryValues[selected] is String) {
+      return categoryValues[selected] as String;
+    } else {
+      throw Exception("Resource $category.$selected is not a value");
+    }
+  }
+
+  String fetch(String key) {
+    final separator = key.lastIndexOf('.');
+    final category = key.substring(0, separator);
+    final selected = key.substring(separator + 1, key.length);
+    final selectedValue = fetchSelectedValue(key, category, selected);
+
+    if (RegExp(numeralAndBracesRegEx).hasMatch(selectedValue)) {
+      return fetchKeyValueData(category, selectedValue);
+    } else if (RegExp(numeralRegEx).hasMatch(selectedValue)) {
+      return fetchNumerals(selectedValue);
+    } else {
+      return selectedValue;
+    }
+  }
+
+  String fetchNumerals(String numeral) {
+    final stringBuffer = StringBuffer();
+    return matchAndReplace(numeral, stringBuffer, numeralOnlyRegEx, (match) {
+      // TODO - { matcher -> matcher.appendReplacement(stringBuffer, Random().nextInt(10).toString()) }
+    });
+  }
+
+  String fetchKeyValueData(String category, String selectedValue) {
+    final stringBuffer = StringBuffer();
+
+    return matchAndReplace(selectedValue, stringBuffer, numeralAndBracesRegEx,
+        (match) {
+      // TODO - { matcher -> matcher.appendReplacement(stringBuffer, fetchValueByCategory(category, matcher.group(1))) }
+    });
+  }
+}
+
+class Params {
+  int separator;
+  String category;
+  bool check;
+  LinkedHashMap<String, LinkedHashMap<String, dynamic>> values;
+
+  Params(
+    this.separator,
+    this.category,
+    this.check,
+    this.values,
+  );
 }
--- test/fakedart_test.dart
+++ test/fakedart_test.dart
@@ -1,13 +1,97 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fakedart/fakedart.dart';
+import 'package:flutter_test/flutter_test.dart' as prefix0;
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+void main() async {
+  Directory current = Directory.current;
+  print(current.path);
+
+  final AssetBundle bundle = TestAssetBundle(<String, List<String>>{
+    'assets/raw/ca.yml': <String>['assets/raw/ca.yml'],
+    'assets/raw/cacat.yml': <String>['assets/raw/cacat.yml'],
+    'assets/raw/dadk.yml': <String>['assets/raw/dadk.yml'],
+    'assets/raw/de.yml': <String>['assets/raw/de.yml'],
+    'assets/raw/deat.yml': <String>['assets/raw/deat.yml'],
+    'assets/raw/dech.yml': <String>['assets/raw/dech.yml'],
+    'assets/raw/en.yml': <String>['assets/raw/en.yml'],
+    'assets/raw/enau.yml': <String>['assets/raw/enau.yml'],
+    'assets/raw/enauocker.yml': <String>['assets/raw/enauocker.yml'],
+    'assets/raw/enbork.yml': <String>['assets/raw/enbork.yml'],
+    'assets/raw/enca.yml': <String>['assets/raw/enca.yml'],
+    'assets/raw/engb.yml': <String>['assets/raw/engb.yml'],
+    'assets/raw/enind.yml': <String>['assets/raw/enind.yml'],
+    'assets/raw/ennep.yml': <String>['assets/raw/ennep.yml'],
+    'assets/raw/enng.yml': <String>['assets/raw/enng.yml'],
+    'assets/raw/ennz.yml': <String>['assets/raw/ennz.yml'],
+    'assets/raw/enpak.yml': <String>['assets/raw/enpak.yml'],
+    'assets/raw/ensg.yml': <String>['assets/raw/ensg.yml'],
+    'assets/raw/enug.yml': <String>['assets/raw/enug.yml'],
+    'assets/raw/enus.yml': <String>['assets/raw/enus.yml'],
+    'assets/raw/enza.yml': <String>['assets/raw/enza.yml'],
+    'assets/raw/es.yml': <String>['assets/raw/es.yml'],
+    'assets/raw/esmx.yml': <String>['assets/raw/esmx.yml'],
+    'assets/raw/fa.yml': <String>['assets/raw/fa.yml'],
+    'assets/raw/fifi.yml': <String>['assets/raw/fifi.yml'],
+    'assets/raw/fr.yml': <String>['assets/raw/fr.yml'],
+    'assets/raw/he.yml': <String>['assets/raw/he.yml'],
+    'assets/raw/id.yml': <String>['assets/raw/id.yml'],
+    'assets/raw/it.yml': <String>['assets/raw/it.yml'],
+    'assets/raw/ja.yml': <String>['assets/raw/ja.yml'],
+    'assets/raw/ko.yml': <String>['assets/raw/ko.yml'],
+    'assets/raw/nbno.yml': <String>['assets/raw/nbno.yml'],
+    'assets/raw/nl.yml': <String>['assets/raw/nl.yml'],
+    'assets/raw/pl.yml': <String>['assets/raw/pl.yml'],
+    'assets/raw/pt.yml': <String>['assets/raw/pt.yml'],
+    'assets/raw/ptbr.yml': <String>['assets/raw/ptbr.yml'],
+    'assets/raw/ru.yml': <String>['assets/raw/ru.yml'],
+    'assets/raw/sk.yml': <String>['assets/raw/sk.yml'],
+    'assets/raw/sv.yml': <String>['assets/raw/sv.yml'],
+    'assets/raw/tr.yml': <String>['assets/raw/tr.yml'],
+    'assets/raw/uk.yml': <String>['assets/raw/uk.yml'],
+    'assets/raw/vi.yml': <String>['assets/raw/vi.yml'],
+    'assets/raw/zhcn.yml': <String>['assets/raw/zhcn.yml'],
+    'assets/raw/zhtw.yml': <String>['assets/raw/zhtw.yml'],
+  });
+
+  test('read yaml assets from file', () async {
+    final fake = Fakedart(bundle: bundle);
+    var values = await fake.getValues("en");
+
+    expect(values, isNot(null));
   });
 }
+
+class TestAssetBundle extends CachingAssetBundle {
+  TestAssetBundle(Map<String, List<String>> assets) : _assets = assets {
+    for (String assetList in assets.keys) {
+      for (String asset in assets[assetList]) {
+        _assetMap[asset] = bytesForFile(asset);
+      }
+    }
+  }
+
+  final Map<String, ByteData> _assetMap = <String, ByteData>{};
+  final Map<String, List<String>> _assets;
+
+  @override
+  Future<ByteData> load(String key) {
+    if (key == 'AssetManifest.json') {
+      return Future<ByteData>.value(bytesForJsonLike(_assets));
+    }
+    return Future<ByteData>.value(_assetMap[key]);
+  }
+}
+
+ByteData bytesForJsonLike(Map<String, dynamic> jsonLike) => ByteData.view(
+    Uint8List.fromList(const Utf8Encoder().convert(json.encode(jsonLike)))
+        .buffer);
+
+ByteData bytesForFile(String path) => ByteData.view(
+    Uint8List.fromList(File('./' + path).readAsBytesSync()).buffer);
